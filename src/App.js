28bfb8228eef27b119ec57b2f657fc954fa508bf
Alexsander Souza <92753962+parvilucifera@users.noreply.github.com>
2022-05-03 13:39:02 -0300

    explanation text and title fix

--- src/App.js
+++ src/App.js
@@ -210,7 +210,14 @@ function App() {
         </Box>
         <Box>
           <Container className="footer">
-            Made by Parvi with React and Chakra UI.{" "}
+          <br />
+            The Pomodoro Technique is a time management method developed by
+            Francesco Cirillo in the late 1980s. It uses a timer to break work
+            into intervals, typically 25 minutes in length, separated by short
+            breaks. Each interval is known as a pomodoro, from the Italian word
+            for tomato, after the tomato-shaped kitchen timer Cirillo used as a
+            university student. - Wikipedia
+            <br /> <br />Made by Parvi with React and Chakra UI.
             <a href="https://github.com/parvilucifera">GitHub</a>.
           </Container>
         </Box>
